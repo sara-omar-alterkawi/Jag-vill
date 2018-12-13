eb6aceaccf5b2fbc66dd2e17462fd2bada223ff9
sara-omar-alterkawi <42577903+sara-omar-alterkawi@users.noreply.github.com>
2018-12-13 09:55:29 +0100

    13-12

--- js/services.js
+++ js/services.js
@@ -6,7 +6,7 @@ app.factory('getAktivities', ['$http', function ($http) {
         inputactivity: function (adress) {
             var adress = adress;
             //https://helsingborg.opendatasoft.com/api/records/1.0/search/?dataset=anlaggningar0&q=Benarpsv%C3%A4gen&facet=adress
-            var url = 'https://helsingborg.opendatasoft.com/api/records/1.0/search/?dataset=anlaggningar0&q='+adress+'&facet=adress';
+            var url = 'https://helsingborg.opendatasoft.com/api/records/1.0/search/?dataset=anlaggningar0&q=' + adress + '&facet=adress';
             return $http.get(url)
                 .then(function (data) {
                     return data.data;
@@ -38,7 +38,7 @@ app.factory('getMuseums', ['$http', function ($http) {
         inputmuseum: function (adress) {
             var adress = adress;
             //https://helsingborg.opendatasoft.com/api/records/1.0/search/?dataset=museer-och-upplevelser&q=Kungsgatan+11
-            var url = 'https://helsingborg.opendatasoft.com/api/records/1.0/search/?dataset=museer-och-upplevelser&q='+ adress;
+            var url = 'https://helsingborg.opendatasoft.com/api/records/1.0/search/?dataset=museer-och-upplevelser&q=' + adress;
             return $http.get(url)
                 .then(function (data) {
                     return data.data;
@@ -53,8 +53,8 @@ app.factory('getSchools', ['$http', function ($http) {
         // All functionality here
         inputschool: function (adress) {
             var adress = adress;
-            //https://helsingborg.opendatasoft.com/api/records/1.0/search/?dataset=museer-och-upplevelser&q=Kungsgatan+11
-            var url = "https://helsingborg.opendatasoft.com/api/records/1.0/search/?dataset=skolor_ny&q="+ adress +"&facet=gatuadress";
+            //https://helsingborg.opendatasoft.com/api/records/1.0/search/?dataset=skolor_ny&q=Svanhalsgatan+27&rows=50&facet=gatuadress
+            var url = "https://helsingborg.opendatasoft.com/api/records/1.0/search/?dataset=skolor_ny&q=" + adress + "&rows=200&facet=gatuadress";
             return $http.get(url)
                 .then(function (data) {
                     return data.data;
